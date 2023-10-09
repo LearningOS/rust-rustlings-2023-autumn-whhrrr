ac09df27e169cf5a287ce934ace69e52e49d76a5
whhrrr <67854308+whhrrr@users.noreply.github.com>
2023-10-09 12:38:28 +0000

    tmp

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,12 +7,12 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
+    // TODO vec0 的所有权是否除了clone还有其它方式可以保护
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0.clone());
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
